7f414d66b503277bf8ff933a8c72f5367746eafa
liggiorgio <liggiorgio@live.com>
2016-06-06 16:32:02 +0200

    Updated graphics, improved CSS

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
     include_once("./config.php");
+
+    if (isset($_SESSION[id])&&($_SESSION['logged']==true))
+        header("./dashboard.php");
+
     include_once("./public/header.php");
     include_once("./public/navbar.php");
 ?>
-
+        <div id="wrapper">
+            <p>Ciaone!</p>
+        </div>
     </body>
 </html>
--- public/navbar.php
+++ public/navbar.php
@@ -1,6 +1,20 @@
 <div id="navbar">
-    <a id="logo" href="./index.php">
+    <a id="logo" href="./index.php" title="Home page">
         <img src="./public/logo.png"/>
         <p>eCommerce</p>
     </a>
+    <div class="ver-bar"></div>
+    <ul>
+        <?php
+            if (isset($_SESSION[id])&&($_SESSION['logged']==true)) {
+                echo '<a><li class="navbutton">Profilo</li></a>';
+                echo '<a><li class="navbutton">Carrello</li></a>';
+            }
+            else {
+                echo '<a href="./login.php" title="Accedi"><li class="navbutton">Accedi</li></a>';
+                echo '<a href="./signup.php" title="Registrati"><li class="navbutton">Registrati</li></a>';
+            }
+        ?>
+        <div style="width: 20px; float:right;" class="ver-bar"></div>
+    </ul>
 </div>
