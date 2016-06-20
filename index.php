5fc5bce3d198c8e90ccd24a3f86abcf7fb99b9f0
liggiorgio <liggiorgio@live.com>
2016-06-21 00:24:59 +0200

    Added articles in home page, fixed CSS

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
     include_once("./config.php");
     include_once("./public/header.php");
     include_once("./public/navbar.php");
+
+    function show_article_thumb($articles) {
+        if ($articles['amount'] > 15) {
+            echo '<a class="article" title="Clicca per dettaglio articolo"><img src="./public/res/articles/'.$articles['id'].'.png"/><br><span class="price">'.$articles['price'].'€</span><span class="amount-available">Q.tà: '.$articles['amount'].'</span><br><br><span class="artname">'.substr($articles['name'],0,77);
+            if (strlen($articles['name']) > 77) {
+                echo '...';
+            }
+            echo '</span></a>';
+        } elseif ($articles['amount'] > 0) {
+            echo '<a class="article" title="Clicca per dettaglio articolo"><img src="./public/res/articles/'.$articles['id'].'.png"/><br><span class="price">'.$articles['price'].'€</span><span class="amount-limited">Q.tà: '.$articles['amount'].'</span><br><br><span class="artname">'.substr($articles['name'],0,77);
+            if (strlen($articles['name']) > 77) {
+                echo '...';
+            }
+            echo '</span></a>';
+        } else {
+            echo '<a class="article" title="Clicca per dettaglio articolo"><img src="./public/res/articles/'.$articles['id'].'.png"/><br><span class="price">'.$articles['price'].'€</span><span class="amount-ended">N/D</span><br><br><span class="artname">'.substr($articles['name'],0,77);
+            if (strlen($articles['name']) > 77) {
+                echo '...';
+            }
+            echo '</span></a>';
+        }
+    }
 ?>
         <div id="wrapper">
             <div id="space-up"></div>
@@ -18,7 +40,7 @@
                 echo '<div id="arts-container"><span class="stretch"></span>';
                     $artsset = mysql_query("SELECT * FROM articles ORDER BY cat");
                     while ($articles = mysql_fetch_array($artsset)) {
-                        echo '<a class="article" title="Clicca per i dettagli"><img src="'.$articles['thumb'].'"/><br><span class="price">'.$articles['price'].'€</span><span class="amount">Disponibili: '.$articles['amount'].'</span><br><br><span class="artname">'.$articles['name'].'</span></a>';
+                        show_article_thumb($articles);
                     }
                 echo '<span class="stretch"></span></div>';
             ?>
